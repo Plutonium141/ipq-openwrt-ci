27ece4acdf84a3428fcc6bed85c9d75fb53c8715
Plutonium141 <62324696+Plutonium141@users.noreply.github.com>
2025-11-07 17:10:21 +0800

    Add fuse dependency for openlist2 compilation fix
    Added fuse dependency to fix openlist2 compilation error.

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -36,6 +36,10 @@ git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat package/kucat/l
 # Go & OpenList & ariang & frp & AdGuardHome & WolPlus & Lucky & OpenAppFilter & 集客无线AC控制器 & 雅典娜LED控制
 git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 package/openlist
+
+# 添加fuse依赖 修复openlist2编译错误
+sed -i 's/PKG_BUILD_DEPENDS:=golang\/host/PKG_BUILD_DEPENDS:=golang\/host fuse/g' package/openlist/openlist2/Makefile
+
 git_sparse_clone ariang https://github.com/laipeng668/packages net/ariang
 git_sparse_clone frp https://github.com/laipeng668/packages net/frp
 mv -f package/frp feeds/packages/net/frp
@@ -45,6 +49,7 @@ mv -f package/luci-app-frps feeds/luci/applications/luci-app-frps
 git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
+
 #git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 #git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
